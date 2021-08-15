a0ecff1897e1f906e2d6501fed89803354518648
sabrilassouli <sabri.lassouli@student.ehb.be>
2021-08-15 21:00:45 +0200

    created class Artikels

--- js/index.js
+++ js/index.js
@@ -4,8 +4,20 @@
 //URL
 //https://thecrew.cc/news/read.php
 
-// CLASS Artikels
+
 // array van artikels fetchen
 // arrays sorten op likes
 // liken functie
 // searchbar toevoegen met search functie
+
+
+class Artikels {
+    constructor(artikelUUID, artikelTitle, artikelImage, artikelContent, artikelLikes, artikelpublicationDate) {
+        this.artikelUUID = artikelUUID;
+        this.artikelTitle = artikelTitle;
+        this.artikelImage = artikelImage;
+        this.artikelContent = artikelContent;
+        this.artikelLikes = artikelLikes;
+        this.artikelpublicationDate = artikelpublicationDate;
+    }
+}
