584c5e53f47227aeace6e3ef3c1a4dd80d5eac34
sabrilassouli <sabri.lassouli@student.ehb.be>
2021-08-15 22:15:40 +0200

    successfuly got the articles on the html page

--- js/index.js
+++ js/index.js
@@ -34,9 +34,36 @@ async function getArtikels() {
     artikelsArray = [];
 
     for (let i = 0; i < arrayLengte; i++) {
-        let artikel = new Artikels(data.news[i].UUID, data.news[i].title, data.news[i].content, data.news[i].imageURI, data.news[i].likes);
+        let artikel = new Artikels(data.news[i].UUID, data.news[i].title, data.news[i].imageURI, data.news[i].content, data.news[i].likes, data.news[i].publicationDate);
         artikelsArray.push(artikel);
     }
+    showArtikles(artikelsArray);
 
 }
 getArtikels();
+
+//putting the article data onto the page
+function showArtikles(ArtikelsData) {
+    
+    let containerElement = document.getElementById('Artikels-container');
+    let htmlContent = "";
+    ArtikelsData.forEach((element) => {
+        htmlContent += `
+            <div>
+            
+            <h1 class="title">${element.artikelTitle}</h1>
+            <img src='${element.artikelImage}' class="artikelImage">
+            <p class="artikelContent">${element.artikelContent}</p>
+            <p class="artikelLikes">${element.artikelLikes}</p>
+            <p class="artikelpublicationDate">${element.artikelpublicationDate}</p>
+             </div>
+             `;
+
+    });
+
+    containerElement.innerHTML = "";
+    containerElement.innerHTML = htmlContent;
+
+  
+
+}
