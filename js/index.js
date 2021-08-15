a60a61841ba9e6b43e439a65a91b2ea37e3a769a
sabrilassouli <sabri.lassouli@student.ehb.be>
2021-08-15 20:53:38 +0200

    added comments to know what i have to do

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 "use strict";
+
+
+//URL
+//https://thecrew.cc/news/read.php
+
+// CLASS Artikels
+// array van artikels fetchen
+// arrays sorten op likes
+// liken functie
+// searchbar toevoegen met search functie
